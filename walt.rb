11c2566daf2715b38b38b32dae19a49341b47883
Adam Edwards <adam.edwards@aver.io>
2016-05-02 20:41:50 -0400

    Add walt homebrew formula.

---
+++ walt.rb
@@ -0,0 +1,14 @@
+class Walt < Formula
+  desc "Build and deploy docker containers"
+  homepage "https://github.com/AverInformatics/aver-tools"
+  url "https://homebrew.avercloud.com/walt/walt.tar.gz"
+  version "0.0.1"
+  #sha256 ""
+
+  def install
+  end
+
+  test do
+      system true
+  end
+end
