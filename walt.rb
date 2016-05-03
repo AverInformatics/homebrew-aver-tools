4c8496422b57a63fc13605f8cb71ad09d75d84ef
Adam Edwards <adam.edwards@aver.io>
2016-05-02 20:58:56 -0400

    Add SHA256 to walt formula; test install command.

--- walt.rb
+++ walt.rb
@@ -3,12 +3,13 @@ class Walt < Formula
   homepage "https://github.com/AverInformatics/aver-tools"
   url "https://homebrew.avercloud.com/walt/walt.tar.gz"
   version "0.0.1"
-  #sha256 ""
+  sha256 "d29bb0d4ee8cd2626fafb1d6fa19ab71f58ccc33a56fc647c160a41ba9061681"
 
   def install
+    bin.install "walt"
   end
 
   test do
-      system true
+    system true
   end
 end
