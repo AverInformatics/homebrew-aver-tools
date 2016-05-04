e45a528cb6b4fcf80366e7b35ec618c74b21a781
Adam Edwards <adam.edwards@aver.io>
2016-05-03 23:31:21 -0400

    Update walt to version 0.1.0

--- walt.rb
+++ walt.rb
@@ -1,9 +1,9 @@
 class Walt < Formula
   desc "Build and deploy docker containers"
   homepage "https://github.com/AverInformatics/aver-tools"
-  url "https://homebrew.avercloud.com/walt/walt.tar.gz"
-  version "0.0.1"
-  sha256 "d29bb0d4ee8cd2626fafb1d6fa19ab71f58ccc33a56fc647c160a41ba9061681"
+  url "https://homebrew.avercloud.com/walt/walt-0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "ad6ed7ee02245fc278c445c42fb5d21e5cb208dedc1f4e8305a5cb76568e7fa9"
 
   def install
     bin.install "walt"
