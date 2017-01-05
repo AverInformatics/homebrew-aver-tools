10b26c18b195d5af49ffbe7d61f8a63bf0a0ff66
Adam Edwards <adam.edwards@aver.io>
2017-01-05 11:32:53 -0500

    Update secrets tool.

--- secrets.rb
+++ secrets.rb
@@ -2,12 +2,12 @@ class Secrets < Formula
   desc "Manage secrets values for Aver applications"
   homepage "https://github.com/AverInformatics/aver-tools"
 
-  # tar czf secrets-0.2.0.tar.gz ./secrets
-  url "https://homebrew.avercloud.com/secrets/secrets-0.2.0.tar.gz"
-  version "0.2.0"
+  # tar czf secrets-0.2.1.tar.gz ./secrets
+  url "https://homebrew.avercloud.com/secrets/secrets-0.2.1.tar.gz"
+  version "0.2.1"
 
-  # openssl dgst -sha256 secrets-0.2.0.tar.gz
-  sha256 "a80e1019571620a7baafc7eda038919fe7d0aa6452d78ddb1546b19bf880d819"
+  # openssl dgst -sha256 secrets-0.2.1.tar.gz
+  sha256 "d2d2abacf20ab874509d6e742a4a8301cefed83c7e788028b06b5a8400590763"
 
   def install
     bin.install "secrets"
